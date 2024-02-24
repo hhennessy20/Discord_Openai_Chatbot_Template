5e6f60238988316d8230e248729081ec8479ca5b
Larry Meng <meng.lawrence@gmail.com>
2024-02-24 14:24:26 -0500

    added comments, commented out debug lines

--- discord_openai_chatbot.py
+++ discord_openai_chatbot.py
@@ -1,3 +1,7 @@
+# Author: hhennessy20 and Shnabbydoodle
+# 2/14/2024
+# Copyright MMXXIV :)
+
 import discord
 import os
 from openai import OpenAI
@@ -17,7 +21,8 @@ def too_many_bots(message_is_bot):
     for message in message_is_bot[-4:]:
         if message[0] and not message[1]:
             counter += 1
-    print(counter)
+    # Debug line to print the counter
+    # print(counter)
     if counter > 2:
         return True
     return False
@@ -53,7 +58,9 @@ def run_bot(names, context_message, openai_key, discord_key):
         # Checks if the message contains name
         if (name_in_message(names, message.content) and message.author != discord_client.user):
 
-            print(message.content)
+            # Debug line to print all messages received by the bot
+            # print(message.content)
+
             # Checks if message is bot
             if message.author.bot and message.author == discord_client.user:
                 message_is_bot.append([True, True])
@@ -65,21 +72,21 @@ def run_bot(names, context_message, openai_key, discord_key):
             if message.author.bot and too_many_bots(message_is_bot):
                 return
             
-            #Gets name or nickname of user to append to bot message
+            # Gets name or nickname of user to append to bot message
             username = message.author.name
             if (message.channel.type is not (discord.ChannelType.private or discord.ChannelType.group)) and message.author.nick is not None:
                 username = message.author.nick
 
-            #Simulates a few seconds of bot reading your message
+            # Simulates a few seconds of bot reading your message
             await asyncio.sleep(random.randint(0,500)/100)
 
-            #Bot will appear to be typing while it generates your message
+            # Bot will appear to be typing while it generates your message
             async with message.channel.typing():
-                #Reminds the bot about itself every 10 messages
+                # Reminds the bot about itself every 10 messages
                 if len(messages) % 10 == 0:
                     messages.append({"role": "system", "content": "Please remember and adhere to the following: " + context_message})
                 
-                #Sends your message to openai and gets response
+                # Sends your message to openai and gets response
                 messages.append({"role": "user", "content": username + " says: " + message.content})
                 chgpt_response = openai_client.chat.completions.create(
                     model="gpt-3.5-turbo",
@@ -89,7 +96,7 @@ def run_bot(names, context_message, openai_key, discord_key):
                 # Adds response to bot context so it remembers
                 messages.append({"role": "assistant", "content": response})
                 
-                #Simulates the bot typing your message, slightly longer depending on the length of the message
+                # Simulates the bot typing your message, slightly longer depending on the length of the message
                 await asyncio.sleep(.01 * len(response))
                 await message.channel.send(response)
 
