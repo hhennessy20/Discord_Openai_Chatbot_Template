a3bef04f06d4fa26446187ed7f445fcd96d7de09
hhennessy20 <hhennessy20@gmail.com>
2024-02-25 21:39:43 -0500

    Test 3...

--- discord_openai_chatbot.py
+++ discord_openai_chatbot.py
@@ -74,7 +74,7 @@ def run_bot(names, context_message, openai_key, discord_key, repetition_interval
                 #random_minute = random.randint(0, 59)
                 #random_second = random.randint(0, 59)
                 random_hour = 21
-                random_minute = 38
+                random_minute = 41
                 random_second = 0
                 scheduled_time = datetime.datetime.now().replace(hour=random_hour, minute=random_minute, second=random_second)
 
@@ -88,21 +88,22 @@ def run_bot(names, context_message, openai_key, discord_key, repetition_interval
                 # Wait until the scheduled time
                 await asyncio.sleep(time_difference.total_seconds())
                 for channel in discord_client.get_all_channels():
-                    if channel.name in propose_conversation_starters_in:
-                        # Get a random non-bot member from the server
-                        member = random.choice([m for m in discord_client.get_all_members() if not m.bot])
-                        username = member.name
-                        if member.nick is not None:
-                            username = member.nick
-                        # Generate a message using OpenAI
-                        generated_message = openai_client.chat.completions.create(
-                            model="gpt-3.5-turbo",
-                            messages=[{"role": "system", "content": "Come up with an interesting conversation starting question and propose it to " + username}]
-                        ).choices[0].message.content
-                        # Send the message to the randomly chosen member
-                        await member.send(generated_message)
-                        # Update the last message time
-                        last_message_time = datetime.datetime.now()
+                    if (channel.type is not (discord.ChannelType.private or discord.ChannelType.group)):
+                        if channel.name in propose_conversation_starters_in:
+                            # Get a random non-bot member from the server
+                            member = random.choice([m for m in discord_client.get_all_members() if not m.bot])
+                            username = member.name
+                            if member.nick is not None:
+                                username = member.nick
+                            # Generate a message using OpenAI
+                            generated_message = openai_client.chat.completions.create(
+                                model="gpt-3.5-turbo",
+                                messages=[{"role": "system", "content": "Come up with an interesting conversation starting question and propose it to " + username}]
+                            ).choices[0].message.content
+                            # Send the message to the randomly chosen member
+                            await member.send(generated_message)
+                            # Update the last message time
+                            last_message_time = datetime.datetime.now()
 
             # Wait for 24 hours before checking again
             await asyncio.sleep(24 * 60 * 60)
